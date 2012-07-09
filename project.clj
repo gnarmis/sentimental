3ddf172f07449c06d83ff95f598cc61c665cf371
Gursimran Singh <g13singh@gmail.com>
2012-07-09 11:06:24 -0500

    added repo url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject sentimental "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject org.clojars.gnarmis/sentimental "0.1.1-SNAPSHOT"
+  :description "A basic sentiment analyzer, based on clojure-opennlp and using the subjectivity lexicon."
+  :url "http://github.com/gnarmis/sentimental"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clojure-opennlp "0.1.10"]
                  [org.clojars.gnarmis/snowball-stemmer "0.1.1-SNAPSHOT"]
